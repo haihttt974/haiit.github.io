156010d7c329436fa423cc6ea6a0dcd04499c0b9
haiit <leduyhai090704@gmail.com>
2026-01-11 08:22:37 +0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: "/",
+  base: "/haiit.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
